2e1ccf9cfeabf19feb4f14a86a517169b2496564
tatsmaki <61022192+tatsmaki@users.noreply.github.com>
2020-10-09 16:08:07 +0300

    fix: map

--- src/index.js
+++ src/index.js
@@ -38,19 +38,11 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    let temp='', array=[];
-    for (let i=0; i<=expr.length; i++) {
-        if (i%10===0 && i!==0) {
-            array.push(temp);
-            temp=expr[i];
-        }
-        else temp+=expr[i];
-    }
-    for (let i=0; i<array.length; i++) {
-        array[i]=array[i].replace(/10/g, ".").replace(/11/g, "-").replace(/0/g, "").replace(/\*{10}/g, " ");
-        if (array[i]!==' ') array[i]=MORSE_TABLE[array[i]];
-    }
-    return array.join('');
+    return expr.match(/.{1,10}/g).map(char => {
+        char=char.replace(/10/g, ".").replace(/11/g, "-").replace(/0/g, "").replace(/\*{10}/g, " ");
+        if(char !== " ") char=MORSE_TABLE[char];
+        return char;
+    }).join('');
 }
 
 module.exports = {
